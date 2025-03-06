3325e60ea063fa887d4facb5016bf29f895b4838
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2025-03-06 21:11:17 +0200

    Styling and button added. Next some icons and more styling.

--- components/AddWorkouts.js
+++ components/AddWorkouts.js
@@ -1,67 +1,80 @@
 import React, { useState } from 'react';
-import { ScrollView, Text, StyleSheet, TextInput, Image, View } from 'react-native';
+import { ScrollView, Text, StyleSheet, TextInput, Image, View, Pressable } from 'react-native';
 
-export default function WdHeader() {
+
+export default function AddWorkouts() {
 
 const [distance, setDistance] = useState('');
 const [time, setTime] = useState('');
-const [day, setDay] = useState('');
+const [day, setDay] = useState(new Date());
+
+const addWorkout = () => {
+  console.log("Workout added", { distance, time, day});
+};
+
 
 
   return (
-    <View style={headerStyles.container}>
-        <Image 
-            style={headerStyles.image} 
-            source={require('../img/running.png')}
-            accessible={true}
-            accessibilityLabel='Running logo'
-            resizeMode="contain"
-        />
+    <View style={addimgStyles.img}>
+      <Image
+        style={addimgStyles.image}
+        source={require('../img/running.png')}
+        accessible={true}
+        accessibilityLabel='Running logo'
+        resizeMode="contain"
+      />
 
-        
-
-        <View>
-          <Text>Distance (km)</Text>
+      <View style={addcontentStyles.container}>
+        <View style={addcontentStyles.inputContainer}>
+          <Text style={addcontentStyles.text}>Distance (km)</Text>
           <TextInput
             value={distance}
             onChangeText={setDistance}
             keyboardType="numeric"
-            placeholder="Enter distance"
+            placeholder="Set"
+            placeholderTextColor="#FFF5E0"
+            style={addcontentStyles.input}
           />
         </View>
 
-        <View>
-          <Text>Time (minutes)</Text>
+        <View style={addcontentStyles.inputContainer}>
+          <Text style={addcontentStyles.text}>Time (minutes)</Text>
           <TextInput
             value={time}
             onChangeText={setTime}
             keyboardType="numeric"
-            placeholder="Enter time"
+            placeholder="Set"
+            placeholderTextColor="#FFF5E0"
+            style={addcontentStyles.input}
           />
         </View>
 
-        <View>
-          <Text>Day</Text>
+        <View style={addcontentStyles.inputContainer}>
+          <Text style={addcontentStyles.text}>Day</Text>
           <TextInput
-            value={day}
-            onChangeText={setDay}
-            keyboardType="numeric"
-            placeholder="Enter Day"
+            value={day.toDateString()}
+            editable={false}
+            style={addcontentStyles.input}
           />
         </View>
+      </View>
 
-    
-
+      <View>
+        <Pressable style={buttonStyles.button} onPress={addWorkout}>
+          <Text style={buttonStyles.buttonText}>ADD WORKOUT</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
+  }
 
-const headerStyles = StyleSheet.create({
-    container: {
+const addimgStyles = StyleSheet.create({
+    img: {
         backgroundColor: '',
         alignItems: 'center', //img center
         justifyContent: 'center',
         padding: 4,
+        
     },
   
     image: {
@@ -69,3 +82,60 @@ const headerStyles = StyleSheet.create({
         height: 60, 
     },
   });
+
+
+
+/* INPUT STYLES */
+const addcontentStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+  },
+
+  inputContainer: {
+    alignItems: 'center',
+    backgroundColor: '#141E46',
+    marginHorizontal: 15,
+    width: '90',
+    height: '120',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+
+  input: {
+    height: 60,
+    padding: 3,
+    textAlign: 'center',
+    color: '#FFF5E0',
+  
+  },
+
+  text: {
+    color: '#FFF5E0',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+/* BUTTON STYLES */
+const buttonStyles = StyleSheet.create({
+
+  button: {
+    width: 210,
+    height: 60,
+    borderRadius: 13,
+    borderWidth: 1,
+    backgroundColor: '#F06543',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 6,
+  },
+
+  buttonText: {
+    color: '#141E46',
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+});
+
